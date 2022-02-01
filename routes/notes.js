204cdb86e9de5b3a4b82083ef11426131a016615
Isaías Reyes León <elreyieon@Isaiass-MacBook-Air.local>
2022-02-01 17:12:29 -0600

    Added delete function. Pending README file.

--- routes/notes.js
+++ routes/notes.js
@@ -1,52 +1,36 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('..//helpers/fsUtils');
 const uuid = require('../helpers/uuid');
-const fs = require('fs');
 
 notes.get('/notes', (req, res) => {
-    readFromFile('db/db.json', 'utf8').then((data) => {
-    console.log(data)
-    return res.json(JSON.parse(data));
-    })
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    });
+
+notes.delete('/notes/:id', (req, res) => {
+    const noteID = req.params.id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const result = json.filter((note) => note.id !== noteID);
+        writeToFile('./db/db.json', result);
+        res.json(`Note ${noteID} has been deleted`);
+    });
 });
-
-
 
 notes.post('/notes', (req, res) => {
     const { title, text } = req.body;
 
-    if (title && text) {
+    if (req.body) {
         const newNote = {
             title,
             text,
             id: uuid(),
         };
-        console.log(newNote)
     readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully`);
     } else {
         res.error('Error in adding note');
     }
 });
-
-// Pending delete function
-notes.delete('/:id', (req, res) => {
-    const noteID = req.params.id;
-    fs.readFile('db/db.json', 'utf8').then((err, data) => {
-        const dataDB = JSON.parse(data);
-    if (err) throw err;
-    else {
-        for (let i = 0; i < dataDB.length; i++) {
-            if (noteID === dataDB[i].id) {
-                dataDB.splice([i], 1)
-                writeToFile('./db/db.json', JSON.stringify(dataDB), (err) => {
-                    if (err) throw err;
-                    return res.json(dataDB)
-                })
-            }
-            }
-        }
-    })
-})
 
 module.exports = notes;
