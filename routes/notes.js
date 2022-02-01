741bb473dbb57e2dc4e1f7875f30928bafafca87
Isaías Reyes León <elreyieon@Isaiass-MacBook-Air.local>
2022-02-01 16:31:20 -0600

    Added routes to display the new notes and save them, pending to add delete function.

--- routes/notes.js
+++ routes/notes.js
@@ -1,26 +1,52 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('..//helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('..//helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+const fs = require('fs');
 
-notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+notes.get('/notes', (req, res) => {
+    readFromFile('db/db.json', 'utf8').then((data) => {
+    console.log(data)
+    return res.json(JSON.parse(data));
+    })
 });
 
-notes.post('./', (req, res) => {
+
+
+notes.post('/notes', (req, res) => {
     const { title, text } = req.body;
 
     if (title && text) {
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuid(),
         };
-
+        console.log(newNote)
     readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully`);
     } else {
         res.error('Error in adding note');
     }
 });
+
+// Pending delete function
+notes.delete('/:id', (req, res) => {
+    const noteID = req.params.id;
+    fs.readFile('db/db.json', 'utf8').then((err, data) => {
+        const dataDB = JSON.parse(data);
+    if (err) throw err;
+    else {
+        for (let i = 0; i < dataDB.length; i++) {
+            if (noteID === dataDB[i].id) {
+                dataDB.splice([i], 1)
+                writeToFile('./db/db.json', JSON.stringify(dataDB), (err) => {
+                    if (err) throw err;
+                    return res.json(dataDB)
+                })
+            }
+            }
+        }
+    })
+})
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
 const PORT = 3001;
-const api = require('./routes/index.js');
+const api = require('./routes/notes.js')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('api', api);
 app.use(express.static('public'));
+app.use('/api', api);
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 
 app.listen(PORT, () => {
