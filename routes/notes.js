16fdb8a505887edb6dd5bf2982d75f841b4f22dd
Isaías Reyes León <elreyieon@Isaiass-MacBook-Air.local>
2022-01-31 20:13:46 -0600

    First commit.

---
+++ routes/notes.js
@@ -0,0 +1,26 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('..//helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('./', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+module.exports = notes;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3001;
+const api = require('./routes/index.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('api', api);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
